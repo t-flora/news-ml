12d2e700e0407afc7503045f8ee85f60f1b41b7f
TiagoFlora <tiagomflora@gmail.com>
2022-02-25 15:45:39 -0800

    add Beta website scraper

---
+++ data/scraper-beta.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, \
+    WebDriverException
+# from selenium.webdriver.common.keys import Keys
+import requests
+from random import randint
+from time import sleep
+from bs4 import BeautifulSoup, SoupStrainer
+import pandas as pd
+from newspaper import Article
+from newspaper import ArticleException
+from newspaper import Config
+from tqdm import tqdm
+
+config = Config()
+config.request_timeout = 10
+
+# browser = webdriver.Chrome("chromedriver.exe")
+
+# browser.get("https://www.beta.team/developments/page/")
+
+url = 'https://www.beta.team/developments/page/'
+scraped_data = []
+
+for page in tqdm(range(1,6)):
+  
+    req = requests.get(url + str(page) + '/')
+    soup = BeautifulSoup(req.text, parse_only=SoupStrainer('a'))
+  
+    links = soup.find_all('a', class_="beta-development-card", attrs={"href"})
+    print("Number of URLs found: ", len(links))
+    for l in links:
+      if l.has_attr("href"):
+        link = l['href']
+        article = Article(link)
+        try:
+          article.download()
+          article.parse()
+          scraped_data.append([link, article.text, article.title, article.publish_date, article.summary])
+        except ArticleException:
+          print("ArticleException raised. Article.download() did not work for this article.")
+    
+    # Stretch goal: download MySQL to store full-text indexed tables and query from them
+
+    sleep(randint(2, 10))
+
+df = pd.DataFrame(scraped_data, columns = ['URL', 'text', 'title', 'published_date', 'summary'])
+df.to_csv("beta_announcements.csv")
